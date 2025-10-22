1daed1ed444e70be82ba91b1ff5307bfa58161c5
dodii11 <Dodikbima11@gmail.com>
2025-10-22 18:42:16 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# warung-se" 
+"# warung-se" 
---
+++ warung-se-fe/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import apiClient from '@/api/axios'; // Sesuaikan path jika perlu
+
+const message = ref('Sedang memuat...');
+
+onMounted(async () => {
+  try {
+    // Kita panggil rute /api/test yang tadi dibuat
+    const response = await apiClient.get('/test');
+    message.value = response.data.message; // "API Warung Ayam Berhasil Terhubung!"
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    message.value = 'Gagal terhubung ke API. Cek CORS.';
+  }
+});
+</script>
+
+<template>
+  <main>
+    <h1>Tes Koneksi API</h1>
+    <p>{{ message }}</p>
+  </main>
+</template>
