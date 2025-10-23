946e8422646aed2cf7468c640b1199b7a5aaba23
Laepher <raphael.rhinovaldi11@gmail.com>
2025-10-23 09:00:00 +0700

    routes update
    update route jadi admin/ dan user/

--- warung-se-be/routes/web.php
+++ warung-se-be/routes/web.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\MenuController;
 use App\Http\Controllers\PesananController;
 
 // MENU
-Route::resource('menu', MenuController::class);
+Route::resource('admin/menu', MenuController::class);
 
 // PESANAN
-Route::get('pesanan', [PesananController::class, 'index'])->name('pesanan.index'); // admin
-Route::get('pesanan-user', [PesananController::class, 'user'])->name('pesanan.user'); // user
-Route::post('pesanan', [PesananController::class, 'store'])->name('pesanan.store');
+Route::get('admin/pesanan', [PesananController::class, 'index'])->name('pesanan.index'); // admin
+Route::get('user/pesanan', [PesananController::class, 'user'])->name('pesanan.user'); // user
+Route::post('admin/pesanan', [PesananController::class, 'store'])->name('pesanan.store');
 Route::post('/pesanan/update-status/{id}', [PesananController::class, 'updateStatus'])->name('pesanan.updateStatus');
-Route::get('pesanan/{id}', [PesananController::class, 'show'])->name('pesanan.show');
+Route::get('admin/pesanan/{id}', [PesananController::class, 'show'])->name('pesanan.show');
