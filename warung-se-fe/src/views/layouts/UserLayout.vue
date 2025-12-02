96a0f69048ae9342a11835d4ee949e8d770dc310
dodii11 <Dodikbima11@gmail.com>
2025-12-02 23:15:30 +0700

    Refactor-Home, Menu, DetailMenu

--- warung-se-fe/src/views/layouts/UserLayout.vue
+++ warung-se-fe/src/views/layouts/UserLayout.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-bg-base">
+  <div class="min-h-screen flex flex-col bg-bg">
     <Navbar />
-
-    <main class="flex-1 max-w-7xl mx-auto p-6 pt-[72px]">
+    <main class="flex-1 max-w-7xl mx-auto p-6 pt-[72px] w-full">
+      <RouterView />
     </main>
 
+    <Footer />
   </div>
 </template>
 
 <script setup>
+import { RouterView } from 'vue-router';
 import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue';
 </script>
